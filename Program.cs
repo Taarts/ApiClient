5852a907d868c16460fa402725cf5141deef1a32
Tricia <tricia@taarts.net>
2022-09-29 11:59:09 -0400

    API is pulling the info as yet unformated

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace ApiClient
 {
@@ -11,8 +15,24 @@ namespace ApiClient
             var client = new HttpClient();
 
             var responseAsString = await client.GetStringAsync("https://ghibliapi.herokuapp.com/films/");
+            // var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsString);
 
             Console.WriteLine(responseAsString);
+            
+            
+            // var table = new ConsoleTable("title", "director", "release date" );
+
+            // foreach (var item in items)
+            // {
+            //     table.AddRow(
+            //         item.title,
+            //         item.director,
+            //         item.release_date
+            //     );
+            //     table.Write();
+            //     {
+                    
+                }
+            }
         }
-    }
-}
+
